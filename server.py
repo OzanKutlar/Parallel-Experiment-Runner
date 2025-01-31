5aa5ab3d04e9f0ebdda35dbcbbafd4f99e963839
Ozan <ozankutlar@protonmail.com>
2025-01-31 14:53:19 +0300

    Bugfix

--- server.py
+++ server.py
@@ -50,6 +50,7 @@ class MyRequestHandler(BaseHTTPRequestHandler):
         
         if self.path == "/logs":
             last_log = int(self.headers.get('lastLog', len(logs) - 6))
+            print(f"Request came for a log with lastlog : {last_log}")
             relevant_logs = logs[last_log+1:last_log+5]
             
             self.send_response(200)
