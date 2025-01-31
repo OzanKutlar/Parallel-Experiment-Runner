1f766380ab582546f2772040e8f58a5e27bc3139
Ozan <ozankutlar@protonmail.com>
2025-01-31 14:33:40 +0300

    Bugfix

--- index.php
+++ index.php
@@ -87,7 +87,7 @@
     
     <script>
         function fetchLogs() {
-            const serverUrl = window.location.hostname + ":3753/logs";
+            const serverUrl = "http://" + window.location.hostname + ":3753/logs";
 
             fetch(serverUrl, {
                 method: 'GET',
