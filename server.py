30f6bb9e464145d2130bc9c17a64c655fec8d114
Ozan <ozankutlar@protonmail.com>
2025-01-31 12:39:21 +0300

    Added getNum endpoint

--- server.py
+++ server.py
@@ -35,6 +35,14 @@ class MyRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         global data_index
         
+        if self.path == "/getNum":
+            # Send the length of data_array as a response
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(str(len(data_array)).encode())
+            return
+        
         computer_name = self.headers.get('ComputerName', '%ComputerName%')
         
         ID = self.headers.get('ID', '-1')
