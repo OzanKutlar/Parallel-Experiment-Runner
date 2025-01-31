3896f8fa7934eaf89b79c87ad453282372b190ce
Ozan <ozankutlar@protonmail.com>
2025-01-31 17:17:19 +0300

    Bugfix

--- index.php
+++ index.php
@@ -143,13 +143,13 @@
 			box.style.transition = 'background-color 1s ease';
 			let textToPut = '';
 			if (state.state === "Finished") {
-				textToPut = `<div><strong>ID</strong>: ${state.index}</div><div><strong>State</strong>: ${state.state}</div><div><strong>PC</strong>: ${state.sentTo}</div><div>${state.index} Finished by ${state.sentTo}</div>`;
+				textToPut = `<div>${state.index} Finished by ${state.sentTo}</div>`;
 				box.style.backgroundColor = 'green';
 			} else if (state.state === "Running") {
-				textToPut = `<div><strong>ID</strong>: ${state.index}</div><div><strong>State</strong>: ${state.state}</div><div><strong>PC</strong>: ${state.sentTo}</div><div>${state.index} Running on ${state.sentTo}</div>`;
+				textToPut = `<div>${state.index} Running on ${state.sentTo}</div>`;
 				box.style.backgroundColor = 'purple';
 			} else if (state.state === "Reset") {
-				textToPut = `<div><strong>ID</strong>: ${state.index}</div><div><strong>State</strong>: ${state.state}</div><div><strong>PC</strong>: ${state.sentTo}</div><div>${state.index} is Waiting</div>`;
+				textToPut = `<div>${state.index} is Waiting</div>`;
 				box.style.backgroundColor = 'red';
 			}
 
