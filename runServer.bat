571e9df99d1684c7971a14d0ba5a7cb156f98cf8
Ozan (Evo) <ozankutlar248@gmail.com>
2025-03-18 11:43:29 +0300

    Added stuff

--- runServer.bat
+++ runServer.bat
@@ -10,13 +10,13 @@ if "%ServiceState%"=="RUNNING" (
     echo ssh-agent service started.
 )
 
-set "CMD1=wsl -u ozan python3 server.py"
-set "CMD2=wsl -u ozan php -S 0.0.0.0:33000"
+set "CMD1=wsl -u ozan --cd %CD% python3 server.py"
+set "CMD2=wsl -u ozan --cd %CD% php -S 0.0.0.0:33000"
 set "CMD3=%CD%/createWebProxy.bat"
 set "CMD4=%CD%/createServerProxy.bat"
 
-set "CMD1=pause"
-set "CMD2=pause"
+REM set "CMD1=pause"
+REM set "CMD2=pause"
 REM set "CMD3=pause"
 REM set "CMD4=pause"
 
@@ -25,3 +25,5 @@ start "" wt new-tab powershell -Command "%CMD1%" ^
 ; split-pane -H powershell -Command "%CMD2%" ^
 ; split-pane -V powershell -Command "%CMD3%" ^
 ; split-pane -V powershell -Command "%CMD4%"
+
+pause
