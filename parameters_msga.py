78052d8b2152c1e99cdcad626199ceba1c21596a
Ozan <ozankutlar@protonmail.com>
2025-04-08 13:46:06 +0300

    Added msga experimental parameters

---
+++ parameters_msga.py
@@ -0,0 +1,51 @@
+shared_params = {
+    "maxFE": [200000],
+    "pop": [100],
+    "func": list(range(1,11)),
+    "algo1": [1,2,3,4],
+    "algo2": [1,2,3,4],
+    "algo3": [1,2,3,4],
+    "algo4": [1,2,3,4]
+}
+
+empty_param = {}
+
+
+
+data_one, id_counter = generate_combined_data(
+    shared_params,
+    id_counter,
+    empty_param
+)
+
+
+seen = set()
+pruned_list = []
+id_counter = 1
+for obj in data_one:
+    # Create and sort the vector
+    vec = sorted([obj['func'], obj['algo1'], obj['algo2'], obj['algo3'], obj['algo4']])
+    vec_tuple = tuple(vec)  # Tuples are hashable and can go in a set
+
+    if vec_tuple in seen:
+        continue
+    else:
+        obj['id'] = id_counter
+        id_counter += 1
+        seen.add(vec_tuple)
+        pruned_list.append(obj)
+
+
+
+
+# data_two, id_counter = generate_combined_data(
+    # shared_params2,
+    # id_counter,
+    # ga_params,
+    # de_params,
+    # pso_params,
+    # bbbc_params
+# )
+
+# data_array = data_one + data_two
+data_array = pruned_list
