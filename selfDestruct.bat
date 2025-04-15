dced89698befcd5d164f6f4b9435f6bc309307ff
Ozan <ozankutlar@protonmail.com>
2025-04-15 14:48:43 +0300

    Bugfix

--- selfDestruct.bat
+++ selfDestruct.bat
@@ -10,11 +10,12 @@ taskkill /F /IM "matlab.exe"
 echo Changing to a temporary directory to allow deletion...
 cd /d "%TEMP%"
 
+echo Scheduling shutdown in 6 minutes.
+shutdown /s /t 360
+
 echo Waiting for 10 seconds before deleting the directory: %TARGET_DIR%
 timeout /t 10 /nobreak >nul
 
+
 echo Deleting directory...
 REM rd /s /q "%TARGET_DIR%"
-
-echo Operation complete.
-shutdown /s /t 360
